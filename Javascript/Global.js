1bfeabe92533137ac2baa5166550d1566b486139
Devesh Khatter <DeveshKhatter@gmail.com>
2023-05-18 00:44:43 -0400

    Navigation Optimized

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -1,7 +1,5 @@
-function loadNavigation()
-{
-    document.getElementById('navigation').innerHTML = `    
-    <nav class="navbar navbar-expand-lg bg-body-tertiary px-4">
+const navbarTemplate = (homeLink, searchLink, topMoviesLink, aboutLink) => `
+  <nav class="navbar navbar-expand-lg bg-body-tertiary px-4">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">FILMHIVE</a>
       <button
@@ -15,68 +13,54 @@ function loadNavigation()
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div
-        class="collapse navbar-collapse justify-content-end"
-        id="navbarNav"
-      >
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="index.html"
-              >Home</a
-            >
+          <li class="nav-item" data-page="home">
+            <a class="nav-link" href="${homeLink}">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pages/search.html">Search</a>
+          <li class="nav-item" data-page="search">
+            <a class="nav-link" href="${searchLink}">Search</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pages/topMovies.html">Top Movies</a>
+          <li class="nav-item" data-page="topmovies">
+            <a class="nav-link" href="${topMoviesLink}">Top Movies</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="pages/About.html">About</a>
+          <li class="nav-item" data-page="about">
+            <a class="nav-link" href="${aboutLink}">About</a>
           </li>
         </ul>
       </div>
     </div>
-  </nav>`
+  </nav>
+`;
+
+function loadNavigation(containerId, currentPage) {
+  let homeLink, searchLink, topMoviesLink, aboutLink;
+
+  if (currentPage === 'home') {
+    homeLink = 'index.html';
+    searchLink = 'pages/search.html';
+    topMoviesLink = 'pages/topMovies.html';
+    aboutLink = 'pages/About.html';
+  } else {
+    homeLink = '../index.html';
+    searchLink = 'search.html';
+    topMoviesLink = 'topMovies.html';
+    aboutLink = 'About.html';
+  }
+
+  document.getElementById(containerId).innerHTML = navbarTemplate(homeLink, searchLink, topMoviesLink, aboutLink);
+
+  setActiveNavItem(currentPage);
 }
-function loadNavigation1()
-{
-    document.getElementById('navigation1').innerHTML = `    
-    <nav class="navbar navbar-expand-lg bg-body-tertiary px-4">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">FILMHIVE</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        class="collapse navbar-collapse justify-content-end"
-        id="navbarNav"
-      >
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="../index.html"
-              >Home</a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="search.html">Search</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="topMovies.html">Top Movies</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="About.html">About</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>`
+
+
+function setActiveNavItem(currentPage) {
+  const navItems = document.querySelectorAll('.nav-item');
+
+  navItems.forEach((navItem) => {
+    const navLink = navItem.querySelector('.nav-link');
+    if (navItem.dataset.page === currentPage) {
+      navLink.classList.add('active');  
+    }
+  });
 }
