f90a78758486611d22c333bd32413f2db7e27c67
3Ramankaur <kau11821@sheridancollege.ca>
2023-05-24 01:29:11 -0400

    Logo updated

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -1,27 +1,26 @@
+
+
 const navbarTemplate = (homeLink, searchLink, topMoviesLink, aboutLink) => `
   <nav class="navbar navbar-expand-lg bg-body-tertiary px-4">
     <div class="container-fluid">
       
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand mx-auto" href="${homeLink}">FILMHIVE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+      <a class="navbar-brand mx-auto" href="${homeLink}"><img src="/Assets/images/Filmhive_logo.png" width=180vh></a>
       <div class="search-icon-link d-lg-none d-xl-none">
       <a href="${searchLink}" class="search-icon-link d-lg-none d-xl-none">
         <i class="bi bi-search"></i> 
       </a>
      </div>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      
-        <ul class="navbar-nav">
+      <div class="offcanvas offcanvas-end " tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+      <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="navbarOffcanvasLgLabel">FILMHIVE</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+
+    <div class="offcanvas-body justify-content-end">
+        <ul class="navbar-nav justify-content-end">
           <li class="nav-item" data-page="home">
             <a class="nav-link" href="${homeLink}">Home</a>
           </li>
@@ -83,3 +82,30 @@ function storeSearchTerm() {
 }
 
 
+
+var appleTouchIcon = document.createElement("link");
+appleTouchIcon.rel = "apple-touch-icon";
+appleTouchIcon.sizes = "180x180";
+appleTouchIcon.href = "/Assets/Favicon/apple-touch-icon.png";
+document.head.appendChild(appleTouchIcon);
+
+var favicon32x32 = document.createElement("link");
+favicon32x32.rel = "icon";
+favicon32x32.type = "image/png";
+favicon32x32.sizes = "32x32";
+favicon32x32.href = "/Assets/Favicon/favicon-32x32.png";
+document.head.appendChild(favicon32x32);
+
+
+var favicon16x16 = document.createElement("link");
+favicon16x16.rel = "icon";
+favicon16x16.type = "image/png";
+favicon16x16.sizes = "16x16";
+favicon16x16.href = "/Assets/Favicon/favicon-16x16.png";
+document.head.appendChild(favicon16x16);
+
+
+var siteManifest = document.createElement("link");
+siteManifest.rel = "manifest";
+siteManifest.href = "/Assets/Favicon/site.webmanifest";
+document.head.appendChild(siteManifest);
--- Javascript/home.js
+++ Javascript/home.js
@@ -13,7 +13,7 @@ function indexload(containerId, currentPage) {
                 <div class="carousel-item ${i === 0 ? 'active' : ''}">
                 <a href = "pages/movieDetails.html">
                     <img src="https://image.tmdb.org/t/p/w500/${topFiveMovies[i].poster_path}" onclick=storeMovieID(${topFiveMovies[i].id}); loadMovieDetails() class="d-block mx-auto my-auto" alt="..." style="max-width: 500px; max-height: 500px;">
-                    <h3 style="text-align:center">${topFiveMovies[i].original_title}<h3>
+                    <h3 onclick=storeMovieID(${topFiveMovies[i].id}) style="text-align:center">${topFiveMovies[i].original_title}<h3>
                 </a>
                     </div>
             `; 
